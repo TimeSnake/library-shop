ea552396fd449287fac64c77b8b58cce64b3970f
funzter <funzter@mail.timesnake.de>
2023-05-18 11:27:34 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/library/shop/CategorySelection.java
+++ src/main/java/de/timesnake/library/shop/CategorySelection.java
@@ -11,102 +11,103 @@ import de.timesnake.basic.bukkit.util.user.inventory.UserInventoryClickEvent;
 import de.timesnake.basic.bukkit.util.user.inventory.UserInventoryClickListener;
 import de.timesnake.basic.bukkit.util.user.inventory.UserInventoryInteractEvent;
 import de.timesnake.basic.bukkit.util.user.inventory.UserInventoryInteractListener;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
 import org.bukkit.Material;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Set;
+public class CategorySelection implements InventoryHolder, UserInventoryInteractListener,
+    UserInventoryClickListener {
 
-public class CategorySelection implements InventoryHolder, UserInventoryInteractListener, UserInventoryClickListener {
+  private static int invSize = 6 * 9;
+  private final ExInventory inventory;
 
-    private static int invSize = 6 * 9;
-    private final ExInventory inventory;
+  private final HashMap<ExItemStack, ShopCategory> shopCategoryByItemId = new HashMap<>();
 
-    private final HashMap<ExItemStack, ShopCategory> shopCategoryByItemId = new HashMap<>();
+  public CategorySelection() {
+    this.inventory = Server.createExInventory(invSize, "Shop", this);
 
-    public CategorySelection(){
-        this.inventory = Server.createExInventory(invSize, "Shop", this);
-
-        for(ShopCategory sc : ShopCategory.values()){
-            inventory.setItemStack(sc.getSlot(), sc.getItem());
-            shopCategoryByItemId.put(sc.getItem(), sc);
-        }
-
-        Server.getInventoryEventManager().addClickListener(this, this);
-        Server.getInventoryEventManager().addInteractListener(this, ShopManager.SHOPSELECTION);
+    for (ShopCategory sc : ShopCategory.values()) {
+      inventory.setItemStack(sc.getSlot(), sc.getItem());
+      shopCategoryByItemId.put(sc.getItem(), sc);
     }
 
+    Server.getInventoryEventManager().addClickListener(this, this);
+    Server.getInventoryEventManager().addInteractListener(this, ShopManager.SHOPSELECTION);
+  }
 
-    @Override
-    public Inventory getInventory() {
-        return inventory.getInventory();
-    }
 
-    @Override
-    public void onUserInventoryInteract(UserInventoryInteractEvent e) {
-        e.getUser().openInventory(getInventory());
-    }
+  @Override
+  public Inventory getInventory() {
+    return inventory.getInventory();
+  }
 
-    @Override
-    public void onUserInventoryClick(UserInventoryClickEvent e) {
-        ExItemStack clickedItem = e.getClickedItem();
-        ShopCategory category = shopCategoryByItemId.get(clickedItem);
+  @Override
+  public void onUserInventoryInteract(UserInventoryInteractEvent e) {
+    e.getUser().openInventory(getInventory());
+  }
 
-        // TODO: Save all created inventories in a hashmap indexed by the user to reduce load
-        ItemShop shop = new ItemShop(e.getUser(), category);
-        e.getUser().openInventory(shop.getInventory());
-    }
+  @Override
+  public void onUserInventoryClick(UserInventoryClickEvent e) {
+    ExItemStack clickedItem = e.getClickedItem();
+    ShopCategory category = shopCategoryByItemId.get(clickedItem);
 
-    public enum ShopCategory{
-        FIREWORK(0, new ExItemStack(Material.FIREWORK_ROCKET).setDisplayName("§cFirework"), "Firework", Set.of(
-                new ExItemStack(Material.REDSTONE).setSlot(0),
-                new ExItemStack(Material.LAPIS_LAZULI).setSlot(1)
+    // TODO: Save all created inventories in a hashmap indexed by the user to reduce load
+    ItemShop shop = new ItemShop(e.getUser(), category);
+    e.getUser().openInventory(shop.getInventory());
+  }
+
+  public enum ShopCategory {
+    FIREWORK(0, new ExItemStack(Material.FIREWORK_ROCKET).setDisplayName("§cFirework"), "Firework",
+        Set.of(
+            new ExItemStack(Material.REDSTONE).setSlot(0),
+            new ExItemStack(Material.LAPIS_LAZULI).setSlot(1)
         )),
-        PETS(1, getPlayerHead("MHF_Fox").setDisplayName("§6Pets"), "Pets", Set.of(
-                getPlayerHead("MHF_Fox").setSlot(0),
-                getPlayerHead("MHF_Sheep").setSlot(1)
-        ));
+    PETS(1, getPlayerHead("MHF_Fox").setDisplayName("§6Pets"), "Pets", Set.of(
+        getPlayerHead("MHF_Fox").setSlot(0),
+        getPlayerHead("MHF_Sheep").setSlot(1)
+    ));
 
-        private final int slot;
-        private final ExItemStack item;
-        private final String name;
-        private final Collection<ExItemStack> offer;
+    private final int slot;
+    private final ExItemStack item;
+    private final String name;
+    private final Collection<ExItemStack> offer;
 
-        ShopCategory(int slot, ExItemStack item, String name, Collection<ExItemStack> offer){
-            this.slot = slot;
-            this.item = item;
-            this.name = name;
-            this.offer = offer;
-        }
-
-        public ExItemStack getItem() {
-            return item;
-        }
-
-        public int getSlot() {
-            return slot;
-        }
-
-        public Collection<ExItemStack> getOffer() {
-            return offer;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        private static ExItemStack getPlayerHead(String name){
-            ExItemStack head = new ExItemStack(Material.PLAYER_HEAD);
-            SkullMeta meta = (SkullMeta) head.getItemMeta();
-            meta.setOwner(name);
-            head.setItemMeta(meta);
-            return head;
-        }
-
-
+    ShopCategory(int slot, ExItemStack item, String name, Collection<ExItemStack> offer) {
+      this.slot = slot;
+      this.item = item;
+      this.name = name;
+      this.offer = offer;
     }
+
+    public ExItemStack getItem() {
+      return item;
+    }
+
+    public int getSlot() {
+      return slot;
+    }
+
+    public Collection<ExItemStack> getOffer() {
+      return offer;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    private static ExItemStack getPlayerHead(String name) {
+      ExItemStack head = new ExItemStack(Material.PLAYER_HEAD);
+      SkullMeta meta = (SkullMeta) head.getItemMeta();
+      meta.setOwner(name);
+      head.setItemMeta(meta);
+      return head;
+    }
+
+
+  }
 
 }
--- src/main/java/de/timesnake/library/shop/ItemShop.java
+++ src/main/java/de/timesnake/library/shop/ItemShop.java
@@ -5,32 +5,32 @@
 package de.timesnake.library.shop;
 
 import de.timesnake.basic.bukkit.util.Server;
+import de.timesnake.basic.bukkit.util.user.User;
 import de.timesnake.basic.bukkit.util.user.inventory.ExInventory;
 import de.timesnake.basic.bukkit.util.user.inventory.ExItemStack;
-import de.timesnake.basic.bukkit.util.user.User;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
 public class ItemShop implements InventoryHolder {
 
-    private final int invSize = 6 * 9;
-    private User user;
-    private ExInventory inventory;
+  private final int invSize = 6 * 9;
+  private User user;
+  private ExInventory inventory;
 
-    public ItemShop(User user, CategorySelection.ShopCategory shopCategory){
-        this.user = user;
+  public ItemShop(User user, CategorySelection.ShopCategory shopCategory) {
+    this.user = user;
 
-        inventory = Server.createExInventory(invSize, shopCategory.getName());
+    inventory = Server.createExInventory(invSize, shopCategory.getName());
 
-        for(ExItemStack item : shopCategory.getOffer()){
-            inventory.setItemStack(item);
-        }
-
+    for (ExItemStack item : shopCategory.getOffer()) {
+      inventory.setItemStack(item);
     }
 
+  }
 
-    @Override
-    public Inventory getInventory() {
-        return inventory.getInventory();
-    }
+
+  @Override
+  public Inventory getInventory() {
+    return inventory.getInventory();
+  }
 }
