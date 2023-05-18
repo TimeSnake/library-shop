6fa66036f760eee05c6393b00817016d92c46a1c
funzter <cavehive@protonmail.com>
2023-05-18 11:27:34 +0200

    reindented files with two spaces

--- src/main/java/de/timesnake/library/shop/ShopManager.java
+++ src/main/java/de/timesnake/library/shop/ShopManager.java
@@ -11,20 +11,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShopManager {
 
-    public static ExItemStack SHOPSELECTION = new ExItemStack(3, Material.ENDER_CHEST, "§dShop")
-            .setMoveable(false).setDropable(false).setLore("§fClick to open the shop");
+  public static ExItemStack SHOPSELECTION = new ExItemStack(3, Material.ENDER_CHEST, "§dShop")
+      .setMoveable(false).setDropable(false).setLore("§fClick to open the shop");
 
-    private final JavaPlugin plugin;
-    private final CategorySelection cs;
+  private final JavaPlugin plugin;
+  private final CategorySelection cs;
 
-    public ShopManager(JavaPlugin plugin) {
-        this.plugin = plugin;
-        Loggers.SHOP.info("Shop loaded");
-        cs = new CategorySelection();
-    }
+  public ShopManager(JavaPlugin plugin) {
+    this.plugin = plugin;
+    Loggers.SHOP.info("Shop loaded");
+    cs = new CategorySelection();
+  }
 
-    public JavaPlugin getPlugin() {
-        return plugin;
-    }
+  public JavaPlugin getPlugin() {
+    return plugin;
+  }
 
 }
